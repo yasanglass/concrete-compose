cca91b19b676e24279b46bacdb8394eb29ee0663
Yasan Glass <contact@yasan.glass>
2023-07-02 12:27:45 +0200

    Improve Text composables

--- stillbirth/src/main/kotlin/glass/yasan/stillbirth/components/Text.kt
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/components/Text.kt
@@ -1,23 +1,28 @@
 package glass.yasan.stillbirth.components
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
-import glass.yasan.spine.compose.type.rubikMonoFamily
+import glass.yasan.spine.compose.type.rubikFamily
 import glass.yasan.stillbirth.theme.StillbirthTheme
 
 @Composable
-public fun TextMonoTitle(
+public fun Text(
     text: String,
+    color: Color,
     modifier: Modifier = Modifier,
     fontSize: TextUnit = TextUnit.Unspecified,
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = rubikFamily,
     letterSpacing: TextUnit = TextUnit.Unspecified,
     textDecoration: TextDecoration? = null,
     textAlign: TextAlign? = null,
@@ -31,11 +36,11 @@ public fun TextMonoTitle(
     Text(
         text = text,
         modifier = modifier,
-        color = StillbirthTheme.textColors.title,
+        color = color,
         fontSize = fontSize,
         fontStyle = fontStyle,
         fontWeight = fontWeight,
-        fontFamily = rubikMonoFamily,
+        fontFamily = fontFamily,
         letterSpacing = letterSpacing,
         textDecoration = textDecoration,
         textAlign = textAlign,
--- stillbirth/src/main/kotlin/glass/yasan/stillbirth/components/TextMonoDescription.kt
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/components/TextMonoDescription.kt
@@ -1,6 +1,5 @@
 package glass.yasan.stillbirth.components
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
