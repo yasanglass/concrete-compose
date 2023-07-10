a6567ca46da315d0e06104e7385666ef1ffae9a7
Yasan Glass <contact@yasan.glass>
2023-07-11 01:12:27 +0200

    Create Bottombar

---
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/components/BottomBar.kt
@@ -0,0 +1,23 @@
+package glass.yasan.stillbirth.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import glass.yasan.stillbirth.theme.StillbirthTheme
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .background(color = StillbirthTheme.colors.layerForeground)
+            .fillMaxWidth(),
+    ) {
+        Divider()
+        content()
+    }
+}
