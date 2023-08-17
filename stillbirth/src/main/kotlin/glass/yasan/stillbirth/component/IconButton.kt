4b0b02dd2cc746331fa32eb8164565165a166917
Yasan Glass <contact@yasan.glass>
2023-08-17 22:28:36 +0200

    Improve IconButton

--- stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/IconButton.kt
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/IconButton.kt
@@ -1,40 +1,52 @@
 package glass.yasan.stillbirth.component
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import glass.yasan.spine.compose.util.adaptiveContentColor
 import glass.yasan.stillbirth.theme.StillbirthTheme
 
 @Composable
 fun IconButton(
     imageVector: ImageVector,
-    onClickLabel: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    tint: Color = StillbirthTheme.colors.content.major,
+    shape: Shape = CircleShape,
+    backgroundColor: Color = StillbirthTheme.colors.layer.foreground,
+    contentColor: Color = adaptiveContentColor(backgroundColor = backgroundColor),
+    borderWidth: Dp? = StillbirthTheme.sizes.borderStrokeWidth,
     enabled: Boolean = true,
-    contentDescription: String?,
 ) {
 
-    Icon(
-        modifier = modifier
-            .padding(4.dp)
-            .clip(CircleShape)
-            .clickable(
-                onClick = onClick,
-                enabled = enabled,
-                onClickLabel = onClickLabel,
-            )
-            .padding(8.dp),
-        imageVector = imageVector,
-        contentDescription = contentDescription,
-        tint = tint,
+    Button(
+        content = {
+            Box(
+                contentAlignment = Alignment.Center,
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(12.dp),
+                    imageVector = imageVector,
+                    contentDescription = null,
+                    tint = contentColor,
+                )
+            }
+        },
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        borderWidth = borderWidth,
+        enabled = enabled,
     )
 }
