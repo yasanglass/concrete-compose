227109dba10f3aa110c86d2b6eb6920c3a319ba3
Yasan Glass <contact@yasan.glass>
2023-09-07 00:15:40 +0200

    Create Icon

---
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/Icon.kt
@@ -0,0 +1,58 @@
+package glass.yasan.stillbirth.component
+
+import androidx.compose.material.Icon as Material3Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import glass.yasan.stillbirth.theme.StillbirthTheme
+
+@Composable
+fun Icon(
+    imageVector: ImageVector,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = StillbirthTheme.colors.content.major,
+) {
+
+    Material3Icon(
+        modifier = modifier,
+        imageVector = imageVector,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun Icon(
+    painter: Painter,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = StillbirthTheme.colors.content.major,
+) {
+
+    Material3Icon(
+        modifier = modifier,
+        painter = painter,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
+
+@Composable
+fun Icon(
+    bitmap: ImageBitmap,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = StillbirthTheme.colors.content.major,
+) {
+
+    Material3Icon(
+        modifier = modifier,
+        bitmap = bitmap,
+        contentDescription = contentDescription,
+        tint = tint,
+    )
+}
