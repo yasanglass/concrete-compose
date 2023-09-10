f7633614b7c9e7fddc152bc51ff9e0499a2560b9
Yasan Glass <contact@yasan.glass>
2023-09-10 21:12:25 +0200

    Improve TextButton

--- stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/TextButton.kt
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/TextButton.kt
@@ -1,26 +1,35 @@
 package glass.yasan.stillbirth.component
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import glass.yasan.spine.compose.util.adaptiveContentColor
 import glass.yasan.stillbirth.theme.StillbirthTheme
+
 
 @Composable
 fun TextButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier,
+    iconStart: ImageVector? = null,
+    iconEnd: ImageVector? = null,
     shape: Shape = RectangleShape,
     backgroundColor: Color = StillbirthTheme.colors.layer.foreground,
     contentColor: Color = adaptiveContentColor(backgroundColor = backgroundColor),
@@ -33,16 +42,37 @@ fun TextButton(
 
     Button(
         content = {
-            Box(
-                contentAlignment = Alignment.Center,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = contentModifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
             ) {
+                if (iconStart != null) {
+                    Icon(
+                        imageVector = iconStart,
+                        contentDescription = null,
+                        tint = it,
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
                 Text(
                     text = text.uppercase(),
                     color = it,
                     fontWeight = FontWeight.Bold,
                     fontSize = 16.sp,
-                    modifier = Modifier.padding(16.dp),
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.Center,
                 )
+                if (iconEnd != null) {
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Icon(
+                        imageVector = iconEnd,
+                        contentDescription = null,
+                        tint = it,
+                    )
+                }
             }
         },
         onClick = onClick,
