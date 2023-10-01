cdee5794635a55f30bc1a32b7f0a11005da5a99c
Yasan Glass <contact@yasan.glass>
2023-10-01 09:13:18 +0200

    Add missing parameters to `Divider`

--- stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/Divider.kt
+++ stillbirth/src/main/kotlin/glass/yasan/stillbirth/component/Divider.kt
@@ -2,17 +2,21 @@ package glass.yasan.stillbirth.component
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import glass.yasan.stillbirth.theme.StillbirthTheme
 import androidx.compose.material3.Divider as Material3Divider
 
 @Composable
 public fun Divider(
     modifier: Modifier = Modifier,
+    color: Color = StillbirthTheme.colors.layer.background,
+    thickness: Dp = StillbirthTheme.sizes.borderStrokeWidth,
 ) {
 
     Material3Divider(
-        color = StillbirthTheme.colors.layer.background,
-        thickness = StillbirthTheme.sizes.borderStrokeWidth,
         modifier = modifier,
+        color = color,
+        thickness = thickness,
     )
 }
