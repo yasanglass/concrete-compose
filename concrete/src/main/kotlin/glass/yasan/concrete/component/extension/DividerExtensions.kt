72f10905f50f47c8ee19346c62d07e42988e7f35
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-02-28 23:49:58 +0100

    Add modifier to divider extension

--- concrete/src/main/kotlin/glass/yasan/concrete/component/extension/DividerExtensions.kt
+++ concrete/src/main/kotlin/glass/yasan/concrete/component/extension/DividerExtensions.kt
@@ -1,10 +1,18 @@
 package glass.yasan.concrete.component.extension
 
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import glass.yasan.concrete.component.Divider
+import glass.yasan.concrete.theme.ConcreteTheme
 
-public fun LazyListScope.divider() {
+public fun LazyListScope.divider(
+    modifier: Modifier = Modifier,
+) {
     item {
-        Divider()
+        Divider(
+            modifier = modifier,
+        )
     }
 }
