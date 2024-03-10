dae8560a076c997daeece79ae832eb3bdc10b14c
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-03-10 02:33:13 +0100

    Create ButtonText

---
+++ concrete/src/main/kotlin/glass/yasan/concrete/component/ButtonText.kt
@@ -0,0 +1,57 @@
+package glass.yasan.concrete.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import glass.yasan.spine.compose.foundation.grid
+
+@Composable
+public fun ButtonText(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    startIcon: ImageVector? = null,
+    endIcon: ImageVector? = null,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        content = { color ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                startIcon?.let { icon ->
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null,
+                        tint = color,
+                        modifier = Modifier.padding(2.grid),
+                    )
+                }
+                Spacer(width = 2.grid)
+                Text(
+                    text = text,
+                    color = color,
+                    modifier = Modifier
+                        .padding(
+                            vertical = 2.grid,
+                        ),
+                )
+                Spacer(width = 2.grid)
+                endIcon?.let { icon ->
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null,
+                        tint = color,
+                        modifier = Modifier.padding(2.grid),
+                    )
+                }
+            }
+        },
+    )
+}
