6235a27f7505944a1d015c26e6b0db8ce4844913
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-03-10 02:33:08 +0100

    Improve Spacer

--- concrete/src/main/kotlin/glass/yasan/concrete/component/Spacer.kt
+++ concrete/src/main/kotlin/glass/yasan/concrete/component/Spacer.kt
@@ -1,11 +1,14 @@
 package glass.yasan.concrete.component
 
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 public fun Spacer(
@@ -17,5 +20,29 @@ public fun Spacer(
         modifier = modifier
             .width(width)
             .height(height),
+    )
+}
+
+@Composable
+public fun RowScope.Spacer(
+    width: Dp,
+    modifier: Modifier = Modifier,
+) {
+    Spacer(
+        width = width,
+        height = 0.dp,
+        modifier = modifier,
+    )
+}
+
+@Composable
+public fun ColumnScope.Spacer(
+    height: Dp,
+    modifier: Modifier = Modifier,
+) {
+    Spacer(
+        width = 0.dp,
+        height = height,
+        modifier = modifier,
     )
 }
