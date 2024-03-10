21aef43b1fe5f8a54782bde8dccfba390e5a0bc5
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-03-10 03:16:43 +0100

    Remove SpacerHorizontal & SpacerVertical

--- concrete/src/main/kotlin/glass/yasan/concrete/component/extension/SpacerExtensions.kt
+++ concrete/src/main/kotlin/glass/yasan/concrete/component/extension/SpacerExtensions.kt
@@ -2,37 +2,16 @@ package glass.yasan.concrete.component.extension
 
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import glass.yasan.concrete.component.Spacer
-import glass.yasan.concrete.component.SpacerHorizontal
-import glass.yasan.concrete.component.SpacerVertical
 
 public fun LazyListScope.spacer(
-    width: Dp,
-    height: Dp,
+    width: Dp = 0.dp,
+    height: Dp = 0.dp,
 ) {
     item {
         Spacer(
             width = width,
-            height = height,
-        )
-    }
-}
-
-public fun LazyListScope.spacerHorizontal(
-    width: Dp,
-) {
-    item {
-        SpacerHorizontal(
-            width = width,
-        )
-    }
-}
-
-public fun LazyListScope.spacerVertical(
-    height: Dp,
-) {
-    item {
-        SpacerVertical(
             height = height,
         )
     }
