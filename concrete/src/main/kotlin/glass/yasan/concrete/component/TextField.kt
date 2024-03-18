e90b0293b190c2014399f0e270c88e8939dc9a95
Yasan Ghaffarian <yasan.ghaffarian@kiwi.com>
2024-03-18 20:44:11 +0100

    Improve TextField

--- concrete/src/main/kotlin/glass/yasan/concrete/component/TextField.kt
+++ concrete/src/main/kotlin/glass/yasan/concrete/component/TextField.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -13,12 +12,13 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
+import glass.yasan.concrete.component.defaults.concreteTextFieldDefaults
 import androidx.compose.material3.TextField as Material3TextField
 
 @Composable
 public fun TextField(
-    value: String,
-    onValueChange: (String) -> Unit,
+    text: String,
+    onTextChange: (String) -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     readOnly: Boolean = false,
@@ -39,11 +39,63 @@ public fun TextField(
     minLines: Int = 1,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     shape: Shape = RectangleShape,
-    colors: TextFieldColors = TextFieldDefaults.colors(),
+) {
+    TextField(
+        text = text,
+        onTextChange = onTextChange,
+        modifier = modifier,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = textStyle,
+        label = label,
+        placeholder = placeholder,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        prefix = prefix,
+        suffix = suffix,
+        supportingText = supportingText,
+        isError = isError,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        minLines = minLines,
+        interactionSource = interactionSource,
+        shape = shape,
+        colors = concreteTextFieldDefaults().colors(),
+    )
+}
+
+@Composable
+public fun TextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+    colors: TextFieldColors,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = RectangleShape,
 ) {
     Material3TextField(
-        value = value,
-        onValueChange = onValueChange,
+        value = text,
+        onValueChange = onTextChange,
         modifier = modifier,
         enabled = enabled,
         readOnly = readOnly,
